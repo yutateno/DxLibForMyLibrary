979761b2c4af0e28013df64f957b2aa028208ff6
YukiNakagawa <kingwasheart@yahoo.co.jp>
2019-11-23 15:26:34 +0900

    実際にテストとしてプロジェクトを作成して実験してみた

---
+++ File/Test_DxLibForMyLibrary_Lib/Test_DxLibForMyLibrary_Lib/main.cpp
@@ -0,0 +1,28 @@
+#include "FileReadWrite_Lib.hpp"
+#include <iostream>
+
+using namespace FileReadWrite_Lib;
+
+
+int main()
+{
+	FileReaderWrite::Init();
+
+	if (!FileReaderWrite::FileCSVRead("test.csv")) return -1;
+
+	std::vector<std::vector<std::string>> map = FileReaderWrite::GetMapData();
+
+	for (int i = 0; i != map.size(); ++i)
+	{
+		for (int j = 0; j != map[i].size(); ++j)
+		{
+			std::cout << map[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+
+
+	if (!FileReaderWrite::FileCSVWrite("test2.csv", map)) return -1;
+
+	return 0;
+}
