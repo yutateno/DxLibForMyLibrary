3049f5adef858b5c67a8ee4275ec5ab5be1c035d
NakagawaYuki <kingwasheart@yahoo.co.jp>
2019-11-30 17:04:27 +0900

    Gameの方で使う2DColl_Libを差し替え

--- Game/2DColl/release/Coll2D_Lib.hpp
+++ Game/2DColl/release/Coll2D_Lib.hpp
@@ -12,9 +12,9 @@ namespace Coll2D_Lib
 		float x, y, r;
 	};
 
-	bool CheckColl(const SBox& t_boxA, const SBox& t_boxB);
+	bool CheckColl(const SBox t_boxA, const SBox t_boxB);
 
-	bool CheckColl(const SCircle& t_circleA, const SCircle& t_circleB);
+	bool CheckColl(const SCircle t_circleA, const SCircle t_circleB);
 
-	bool CheckColl(const SBox& t_box, const SCircle& t_circle);
+	bool CheckColl(const SBox t_box, const SCircle t_circle);
 }
