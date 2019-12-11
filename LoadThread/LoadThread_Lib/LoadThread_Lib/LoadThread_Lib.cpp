514f5eff49383c6c155c886b51986692e78b0df3
NakagawaYuki <kingwasheart@yahoo.co.jp>
2019-12-12 05:09:01 +0900

    非同期処理ロードを作成したが、使う上で調整が必要な点がある。 一つ、ロード画面はスタティック関数。 二つ、クラスポインタとして扱う。

--- LoadThread/LoadThread_Lib/LoadThread_Lib/LoadThread_Lib.cpp
+++ LoadThread/LoadThread_Lib/LoadThread_Lib/LoadThread_Lib.cpp
@@ -1,2 +1,158 @@
 #include "pch.h"
 #include "LoadThread_Lib.hpp"
+
+
+namespace LoadThread_Lib
+{
+	using namespace std;
+	mutex mtx;
+
+
+	/// ---------------------------------------------------------------------------------------
+	void LoadThread::MyNextLoad(const std::string path, int& file, const ELOADFILE type)
+	{
+		lock_guard<mutex> lock(mtx);
+
+		MyLoad(path, file, type);
+
+		ClearDrawScreen();
+		m_function(num++, maxNum);
+		ScreenFlip();
+	}
+
+
+
+	/// ---------------------------------------------------------------------------------------
+	void LoadThread::MyLoad(const string path, int& file, const ELOADFILE type)
+	{
+		// ロードする
+		switch (type)
+		{
+			// UI関係の画像のとき
+		case ELOADFILE::graph:
+			file = LoadGraph(path.c_str());
+			break;
+
+
+			// 2D系SEのとき
+		case ELOADFILE::soundEffect:
+			file = LoadSoundMem(path.c_str());
+			break;
+
+
+			// モデルデータのとき
+		case ELOADFILE::model:
+			file = MV1LoadModel(path.c_str());
+			break;
+
+
+			// BGMのとき
+		case ELOADFILE::backGroundMusic:
+			SetCreateSoundDataType(DX_SOUNDDATATYPE_FILE);
+			file = LoadSoundMem(path.c_str());
+			SetCreateSoundDataType(DX_SOUNDDATATYPE_MEMNOPRESS);
+			break;
+
+
+			// 3Dサウンドのとき
+		case ELOADFILE::sound3DEffect:
+			SetCreate3DSoundFlag(TRUE);
+			file = LoadSoundMem(path.c_str());
+			SetCreate3DSoundFlag(FALSE);
+			break;
+
+
+		default:
+			break;
+		}
+	}
+
+
+
+	/// ---------------------------------------------------------------------------------------
+	LoadThread::LoadThread()
+	{
+		num = 0;
+		maxNum = 0;
+		end = false;
+
+		std::vector<int>().swap(loadData);
+		std::vector<ELOADFILE>().swap(loadType);
+	}
+
+
+
+	/// ---------------------------------------------------------------------------------------
+	LoadThread::~LoadThread()
+	{
+		for (int i = 0; i < loadType.size(); ++i)
+		{
+			switch (loadType.at(i))
+			{
+				// UI関係の画像のとき
+			case ELOADFILE::graph:
+				DeleteGraph(loadData.at(i));
+				break;
+
+
+				// 2D系SEのとき
+			case ELOADFILE::soundEffect:
+				DeleteSoundMem(loadData.at(i));
+				break;
+
+
+				// モデルデータのとき
+			case ELOADFILE::model:
+				MV1DeleteModel(loadData.at(i));
+				break;
+
+
+				// BGMのとき
+			case ELOADFILE::backGroundMusic:
+				DeleteSoundMem(loadData.at(i));
+				break;
+
+
+				// 3Dサウンドのとき
+			case ELOADFILE::sound3DEffect:
+				DeleteSoundMem(loadData.at(i));
+				break;
+
+
+			default:
+				break;
+			}
+		}
+		std::vector<int>().swap(loadData);
+		std::vector<ELOADFILE>().swap(loadType);
+	}
+
+
+
+	/// ---------------------------------------------------------------------------------------
+	void LoadThread::Process(const int max, const std::vector<std::string> path, const std::vector<ELOADFILE> type)
+	{
+		if (end) return;
+
+		maxNum = max;
+		loadData.resize(max);
+
+		for (int i = 0; i < max; ++i)
+		{
+			ths.push_back(thread(&LoadThread::MyNextLoad, this, path.at(i), ref(loadData.at(i)), type.at(i)));
+			ths.at(i).join();
+		}
+
+		loadType = type;
+
+		end = true;
+	}
+
+
+
+	/// ---------------------------------------------------------------------------------------
+	void LoadThread::SetLoadFunc(std::function<void(const int, const int)> t_loadFunc)
+	{
+		m_function = t_loadFunc;
+	}
+}
--- LoadThread/LoadThread_Lib/LoadThread_Lib/LoadThread_Lib.hpp
+++ LoadThread/LoadThread_Lib/LoadThread_Lib/LoadThread_Lib.hpp
@@ -1,5 +1,77 @@
 #pragma once
-class LoadThread_Lib
-{
-};
 
+#ifndef _LOADTHREAD_LIB_HPP
+#define _LOADTHREAD_LIB_HPP
+
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <functional>
+
+#include "DxLib.h"
+
+
+
+namespace LoadThread_Lib
+{
+	// 読み込むファイルの種類
+	enum class ELOADFILE { graph, soundEffect, model, backGroundMusic, sound3DEffect };
+
+	
+
+	class LoadThread
+	{
+	private:
+		// 非同期を行う
+		std::vector<std::thread> ths;
+
+		// ロードした個数
+		int num;
+
+		// 最大ロード個数
+		int maxNum;
+
+		// ロードしたもの
+		std::vector<int> loadData;
+
+		// ロードしたもののタイプ
+		std::vector<ELOADFILE> loadType;
+
+		// 非同期を行うメソッド
+		void MyNextLoad(const std::string path, int& file, const ELOADFILE type);
+
+		// 終了したかどうか
+		bool end;
+
+		// ロード画面
+		std::function<void(const int, const int)> m_function;
+
+		// メディアのロードを行う
+		void MyLoad(const std::string path, int& file, const ELOADFILE type);
+
+
+	public:
+		// コンストラクタ
+		LoadThread();
+
+		// デストラクタ
+		~LoadThread();
+
+
+		// プロセス
+		void Process(const int max, const std::vector<std::string> path, const std::vector<ELOADFILE> type);
+
+
+		// ロードしたものを渡す
+		const std::vector<int> GetFile() const { return loadData; }
+
+		// ロード終えたかどうか
+		const bool GetEnd() const { return end; }
+
+		// ロード画面を貰う(ややこしかったのでスタティック関数ポインタが好ましい)
+		void SetLoadFunc(std::function<void(const int, const int)> t_loadFunc);
+	};
+}
+
+#endif
